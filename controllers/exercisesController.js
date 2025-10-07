fd854dd6ea515b58e65d7fbcb27887dc2f9eab85
Karim Kanaan <karim.kanaan@qubersavings.com>
2025-10-07 15:58:18 -0300

    exercises now render in views using hbs

--- controllers/exercisesController.js
+++ controllers/exercisesController.js
@@ -1,25 +1,6 @@
 const express = require('express');
 const axios = require('axios');
 
-
-// this is just there so I can understand it more
-// const fetchAllExercies = async (req , res) => {
-//     try {
-//         const response = await axios.get(`${process.env.BASE_URL}/exercises`, {
-//             headers: {
-//                 "X-RapidAPI-Key": process.env.API_KEY,
-//             }
-//         });
-//         console.log(response.data);
-//         res.status(201).json(response.data);
-
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({'error' : "There was an unexpected error fetching list of exercises"});
-//     }
-// }
-
-
 /**
  * This api request returns a list of exercises from gym workouts
  * @param {*} req 
@@ -30,17 +11,21 @@ const fetchAllExercies = async (req, res) => {
     await axios.get(`${process.env.BASE_URL}/exercises?limit=0`, {
         headers: {
             "X-RapidAPI-Key": process.env.API_KEY,
-            "X-RapidAPI-Host" : "exercisedb.p.rapidapi.com"
+            "X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
         }
     })
         .then(response => {
             console.log(response.data);
-            res.status(201).json(response.data);
+            const exercises = response.data;
+            res.render('exercises', {
+                title: "All Exercises",
+                exercises: exercises.slice(0, 20) // optional: limit to 20 for page
+            });
         }
         )
         .catch(err => {
             console.error(err);
-            res.status(500).json({"errorMessage" : "There has been an error fetching the exercises"});
+            res.status(500).json({ "errorMessage": "There has been an error fetching the exercises" });
         });
 
 }
--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
+const path = require('path');
 
-app.use('/exercises', require('./routes/exercisesRoutes'))
+app.set('views' , path.join(__dirname , 'views'));
+app.set('view engine' , 'hbs');
+
+app.set('view options' , {
+    layout: 'layout'
+})
+
+app.use('/exercises', require('./routes/exercisesRoutes'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 
 app.listen(port, () => {
     console.log(`Server running at port ${port}`);
